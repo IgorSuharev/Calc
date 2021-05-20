2cafcf4a3146cef888ace7147a4735e3bb6feab6
Игорь Сухарев <new.igorsuharev@gmail.com>
2021-05-21 02:24:53 +0300

    replace copy inits with uniform-inits. rename defineName() -> declare(). replace C-style casts with static_casts. make reachedTerminalToken() const.

--- Parser.cpp
+++ Parser.cpp
@@ -53,8 +53,8 @@ Parser::Parser(TokenStream& tokenStream, const VariableTable& variableTable)
 #pragma warning(disable: 4061 4062)
 
 double Parser::statement() {
-    Token token = tokenStream.get();
-    double result = 0;
+    Token token{ tokenStream.get() };
+    double result{ 0 };
 
     switch (token.getType()) {
     case TokenType::exit:
@@ -65,7 +65,7 @@ double Parser::statement() {
         result = declaration();
         break;
     case TokenType::name: {
-        Token token2 = tokenStream.get();
+        Token token2{ tokenStream.get() };
         tokenStream.putback(token2);
         tokenStream.putback(token);
         if (token2.getType() == TokenType::equal) {
@@ -82,58 +82,58 @@ double Parser::statement() {
     token = tokenStream.get();
     if (token.getType() != TokenType::statementEnding) {
         tokenStream.putback(token);
-        throw runtime_error(Token::typeToValue(TokenType::statementEnding) + " expected");
+        throw runtime_error{ Token::typeToValue(TokenType::statementEnding) + " expected" };
     }
     return result;
 }
 
 double Parser::declaration() {
     // read declaration keyword
-    Token token = tokenStream.get();
+    Token token{ tokenStream.get() };
     if (token.getType() != TokenType::declarationKeyword) {
         tokenStream.putback(token);
-        throw runtime_error(Token::typeToValue(TokenType::declarationKeyword) + " expected");
+        throw runtime_error{ Token::typeToValue(TokenType::declarationKeyword) + " expected" };
     }
 
     // read variable name
     token = tokenStream.get();
     if (token.getType() != TokenType::name) {
         tokenStream.putback(token);
-        throw runtime_error("variable name expected");
+        throw runtime_error{ "variable name expected" };
     }
-    string name = token.getValue();
+    string name{ token.getValue() };
 
     // read equal sign
     token = tokenStream.get();
     if (token.getType() != TokenType::equal) {
         tokenStream.putback(token);
-        throw runtime_error(Token::typeToValue(TokenType::equal) + " expected");
+        throw runtime_error{ Token::typeToValue(TokenType::equal) + " expected" };
     }
 
-    return variableTable.defineName(name, expression());
+    return variableTable.declare(name, expression());
 }
 
 double Parser::assignment() {
     // read variable name
-    Token token = tokenStream.get();
+    Token token{ tokenStream.get() };
     if (token.getType() != TokenType::name)	{
         tokenStream.putback(token);
-        throw runtime_error("variable name expected");
+        throw runtime_error{ "variable name expected" };
     }
-    string name = token.getValue();
+    string name{ token.getValue() };
 
     // read equal sign
     token = tokenStream.get();
     if (token.getType() != TokenType::equal) {
         tokenStream.putback(token);
-        throw runtime_error(Token::typeToValue(TokenType::equal) + " expected");
+        throw runtime_error{ Token::typeToValue(TokenType::equal) + " expected" };
     }
     return variableTable.assign(name, expression());
 }
 
 double Parser::expression() {
-    double left = term();
-    Token token = tokenStream.get();
+    double left{ term() };
+    Token token{ tokenStream.get() };
 
     while (true) {
         switch (token.getType()) {
@@ -152,17 +152,16 @@ double Parser::expression() {
 }
 
 double Parser::term() {
-    double left = secondary();
-    Token token = tokenStream.get();
+    double left{ secondary() };
+    Token token{ tokenStream.get() };
 
     while (true) {
         switch (token.getType()) {
         case TokenType::multiply:
             left *= secondary();
             break;
-        case TokenType::divide:
-        {
-            double right = secondary();
+        case TokenType::divide: {
+            double right{ secondary() };
             if (right == 0) {
                 left = numeric_limits<double>().quiet_NaN();
             } else {
@@ -182,8 +181,8 @@ double Parser::term() {
 }
 
 double Parser::secondary() {
-    double left = primary();
-    Token token = tokenStream.get();
+    double left{ primary() };
+    Token token{ tokenStream.get() };
 
     while (true) {
         switch (token.getType()) {
@@ -191,7 +190,7 @@ double Parser::secondary() {
             if (isnan(left) || left < 0) {
                 left = isnan(left);
             } else {
-                left = double(Math::factorial(unsigned(left)));
+                left = static_cast<double>(Math::factorial(static_cast<unsigned>(left)));
             }
             break;
         default:
@@ -203,15 +202,15 @@ double Parser::secondary() {
 }
 
 double Parser::primary() {
-    Token token = tokenStream.get();
+    Token token{ tokenStream.get() };
     switch (token.getType()) {
     case TokenType::leftParenthesis:
     {
-        double expr = expression();
+        double expr{ expression() };
         token = tokenStream.get();
         if (token.getType() != TokenType::rightParenthesis) {
             tokenStream.putback(token);
-            throw runtime_error("')' expected");
+            throw runtime_error{ "')' expected" };
         }
         return expr;
     }
@@ -225,11 +224,11 @@ double Parser::primary() {
         return -primary();
     default:
         tokenStream.putback(token);
-        throw runtime_error("primary expected");
+        throw runtime_error{ "primary expected" };
     }
 }
 
-bool Parser::reachedTerminalToken() {
+bool Parser::reachedTerminalToken() const {
     return field_reachedTerminalToken;
 }
 
